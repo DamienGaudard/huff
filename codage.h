7128564e7d8a0505043a6cef3b25b4da0b4b79d6
projetalgo1 <damien.gaudard06@gmail.com>
2017-02-08 23:08:15 +0100

    quatrieme

---
+++ codage.h
@@ -0,0 +1,32 @@
+/**
+ * Polytech Marseille
+ * Case 925 - 163, avenue de Luminy
+ * 13288 Marseille CEDEX 9
+ *
+ * Ce fichier est l'oeuvre d'eleves de Polytech Marseille. Il ne peut etre
+ * reproduit, utilise ou modifie sans l'avis express de ses auteurs.
+ */
+
+/**
+ * @author GAUDARD Damien <damien.gaudard@etu.univ-amu.fr>
+ * @author AIT SAID Anaïs <anais.ait-said@etu.univ-amu.fr>
+ */
+
+
+/**
+ * Projet algorithmique
+ */
+
+
+#ifndef __Projet_Algo__codage__
+#define __Projet_Algo__codage__
+#include <stdio.h>
+#include "arbres.h"
+#include "fichier.h"
+
+
+tpa encoder(tpa a);
+
+tpa decoder(tpa a);
+
+#endif
